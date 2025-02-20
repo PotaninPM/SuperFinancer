57b588d115bf82d5ef71a385b87181fe8f4185bf
PotaninPM <potaninpm@gmail.com>
2025-02-20 14:39:17 +0300

    fix: little change in strings

--- feature_finances/src/main/java/com/potaninpm/feature_finances/presentation/components/goals/goalCard/GoalTextInfo.kt
+++ feature_finances/src/main/java/com/potaninpm/feature_finances/presentation/components/goals/goalCard/GoalTextInfo.kt
@@ -3,9 +3,7 @@ package com.potaninpm.feature_finances.presentation.components.goals.goalCard
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
